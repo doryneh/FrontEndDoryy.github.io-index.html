520092104960013fd535ab200d4e577ea4b73013
doryneh21@gmail.com <doryneh21@gmail.com>
2021-04-20 23:09:27 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,29 @@
+$(window).scroll(function () {
+  if ($(this).scrollTop() >= 650) {
+    $("#navbar").addClass("noTransparrent");
+  } else {
+    $("#navbar").removeClass("noTransparrent");
+  }
+ 
+});
+     $(window).one('scroll', () => {
+        $('.circle')
+          .circleProgress({
+            startAngle: -Math.PI / 2,
+            fill: '#4fb9ff'
+          })
+          .on('circle-animation-progress', function (event, progress, stepValue) {
+            $(this)
+              .find('span')
+              .html(Math.round(stepValue * 100) + '%');
+          });
+      });
+$(function () {
+   $("#commentForm").validate();
+});
+$(document).ready(function(){
+  $(".nav-item").click(function(){
+      $(".nav-item").removeClass("active");
+      $(this).addClass("active");
+  });
+});
