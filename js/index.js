b540474b283fd4abf4ce15062446ddaf5bba080f
doryneh21@gmail.com <doryneh21@gmail.com>
2021-06-06 15:56:46 +0300

    first commit

--- js/index.js
+++ js/index.js
@@ -3,14 +3,13 @@ $(window).scroll(function () {
     $("#navbar").addClass("noTransparrent");
   } else {
     $("#navbar").removeClass("noTransparrent");
-  }
- 
+  } 
 });
      $(window).one('scroll', () => {
         $('.circle')
           .circleProgress({
             startAngle: -Math.PI / 2,
-            fill: '#4fb9ff'
+            fill: '#bd40b0'
           })
           .on('circle-animation-progress', function (event, progress, stepValue) {
             $(this)
@@ -26,4 +25,35 @@ $(document).ready(function(){
       $(".nav-item").removeClass("active");
       $(this).addClass("active");
   });
+});
+
+const mandrill = require('node-mandrill')('<your API Key>'); 
+
+function sendEmail ( _name, _email, _subject, _message) {
+    mandrill('/messages/send', {
+        message: {
+            to: [{email: _email , name: _name}],
+            from_email: document.getElementById("cemail").value,
+            subject: _subject,
+            text: _message
+        }
+    }, function(error, response){
+        if (error) console.log( error );
+        else console.log(response);
+    });
+}
+
+// define your own email api which points to your server.
+
+app.post( '/api/sendemail/', function(req, res){
+            
+    let _name = req.body.name;
+    let _email = req.body.email;
+    let _subject = req.body.subject;
+    let _messsage = req.body.message;
+
+    //implement your spam protection or checks. 
+
+    sendEmail ( _name, _email, _subject, _message );
+
 });
