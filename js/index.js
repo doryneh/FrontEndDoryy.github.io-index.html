b30c9da4982d134851f453265453cb559b33b196
doryneh21@gmail.com <doryneh21@gmail.com>
2021-06-06 16:23:42 +0300

    first commit

--- js/index.js
+++ js/index.js
@@ -26,34 +26,3 @@ $(document).ready(function(){
       $(this).addClass("active");
   });
 });
-
-const mandrill = require('node-mandrill')('<your API Key>'); 
-
-function sendEmail ( _name, _email, _subject, _message) {
-    mandrill('/messages/send', {
-        message: {
-            to: [{email: _email , name: _name}],
-            from_email: document.getElementById("cemail").value,
-            subject: _subject,
-            text: _message
-        }
-    }, function(error, response){
-        if (error) console.log( error );
-        else console.log(response);
-    });
-}
-
-// define your own email api which points to your server.
-
-app.post( '/api/sendemail/', function(req, res){
-            
-    let _name = req.body.name;
-    let _email = req.body.email;
-    let _subject = req.body.subject;
-    let _messsage = req.body.message;
-
-    //implement your spam protection or checks. 
-
-    sendEmail ( _name, _email, _subject, _message );
-
-});
