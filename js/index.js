497b05226c5245538db872d99029438d38eece38
dory.nehme <79232144+doryneh@users.noreply.github.com>
2022-05-31 15:26:06 +0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -3,27 +3,27 @@ $(window).scroll(function () {
     $("#navbar").addClass("noTransparrent");
   } else {
     $("#navbar").removeClass("noTransparrent");
-  } 
+  }
 });
-     $(window).one('scroll', () => {
-        $('.circle')
-          .circleProgress({
-            startAngle: -Math.PI / 2,
-            fill: '#2A2A2A'
-          })
-          .on('circle-animation-progress', function (event, progress, stepValue) {
-            $(this)
-              .find('span')
-              .html(Math.round(stepValue * 100) + '%');
-          });
-      });
+$(window).one('scroll', () => {
+  $('.circle')
+    .circleProgress({
+      startAngle: -Math.PI / 2,
+      fill: '#2A2A2A'
+    })
+    .on('circle-animation-progress', function (event, progress, stepValue) {
+      $(this)
+        .find('span')
+        .html(Math.round(stepValue * 100) + '%');
+    });
+});
 $(function () {
-   $("#commentForm").validate();
+  $("#commentForm").validate();
 });
-$(document).ready(function(){
-  $(".nav-item").click(function(){
-      $(".nav-item").removeClass("active");
-      $(this).addClass("active");
+$(document).ready(function () {
+  $(".nav-item").click(function () {
+    $(".nav-item").removeClass("active");
+    $(this).addClass("active");
   });
 });
 
